33121633592728364bd5a061ec0dd6fdc532f913
Cedric Ong <cedricongjh@gmail.com>
2023-03-18 23:24:08 +0800

    add tests for getTeamsForSection and getTeamsForCourse

--- src/test/java/teammates/sqllogic/core/FeedbackQuestionsLogicTest.java
+++ src/test/java/teammates/sqllogic/core/FeedbackQuestionsLogicTest.java
@@ -182,6 +182,7 @@ public class FeedbackQuestionsLogicTest extends BaseTestCase {
         List<FeedbackQuestion> expectedQuestions = List.of(fq1, fq2);
 
         List<FeedbackQuestion> actualQuestions = fqLogic.getFeedbackQuestionsForStudents(fs);
+
         assertEquals(expectedQuestions.size(), actualQuestions.size());
         assertTrue(actualQuestions.containsAll(actualQuestions));
     }
@@ -196,6 +197,7 @@ public class FeedbackQuestionsLogicTest extends BaseTestCase {
         List<FeedbackQuestion> expectedQuestions = List.of(fq3, fq4, fq5);
 
         List<FeedbackQuestion> actualQuestions = fqLogic.getFeedbackQuestionsForInstructors(fs, "instr1@teammates.tmt");
+        
         assertEquals(expectedQuestions.size(), actualQuestions.size());
         assertTrue(actualQuestions.containsAll(actualQuestions));
     }
