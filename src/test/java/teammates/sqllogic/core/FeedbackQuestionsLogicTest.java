9167c044d60fdd409e55290b162741b39767200c
Cedric Ong <cedricongjh@gmail.com>
2023-03-19 04:01:44 +0800

    fix checkstyleissues

--- src/test/java/teammates/sqllogic/core/FeedbackQuestionsLogicTest.java
+++ src/test/java/teammates/sqllogic/core/FeedbackQuestionsLogicTest.java
@@ -197,7 +197,7 @@ public class FeedbackQuestionsLogicTest extends BaseTestCase {
         List<FeedbackQuestion> expectedQuestions = List.of(fq3, fq4, fq5);
 
         List<FeedbackQuestion> actualQuestions = fqLogic.getFeedbackQuestionsForInstructors(fs, "instr1@teammates.tmt");
-        
+
         assertEquals(expectedQuestions.size(), actualQuestions.size());
         assertTrue(actualQuestions.containsAll(actualQuestions));
     }
